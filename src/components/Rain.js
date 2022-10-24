a61fa2ff894d7e2fe01c5f5e7fa1ce1243627fb0
jmo5896 <justin.m.moore5896@gmail.com>
2022-10-24 12:31:57 -0500

    finally got the rain to fallnpm start!

--- src/components/Rain.js
+++ src/components/Rain.js
@@ -1,26 +1,42 @@
-import React from "react";
-// rainCount start value 15000
+import React, { useRef, useMemo } from "react";
+import { useFrame } from "react-three-fiber";
 
+// rainCount start value 15000
 export default function Rain({ rainCount }) {
-  //   console.log(rainCount);
-  const drops = new Float32Array(
-    [...Array(rainCount * 3).keys()].map((i) => {
-      if (i % 2 === 0) {
-        return Math.random() * 500 - 250;
+  let positions = useMemo(() => {
+    let positions = [];
+    for (let i = 0; i < rainCount; i++) {
+      positions.push(Math.random() * 400 - 200);
+      positions.push(Math.random() * 500 - 250);
+      positions.push(Math.random() * 400 - 200);
+    }
+
+    return new Float32Array(positions);
+  });
+  const myDrops = useRef();
+
+  useFrame(() => {
+    const positions = myDrops.current.array;
+    // array for updating JUST the y values, 2, 5, 8, 11, etc...
+    for (let i = 1; i < positions.length; i += 3) {
+      positions[i] -= 1;
+      // resets the drops so they fall in an infinite manor
+      if (positions[i] < 0) {
+        positions[i] = Math.random() * 500 - 250;
       }
-      return Math.random() * 400 - 200;
-    })
-  );
+    }
+    myDrops.current.needsUpdate = true;
+  });
   // console.log(drops);
   return (
     <points>
-      <bufferGeometry>
+      <bufferGeometry attach="geometry">
         <bufferAttribute
+          ref={myDrops}
           attach="attributes-position"
-          count={drops.length / 3}
-          array={drops}
+          count={positions.length / 3}
+          array={positions}
           itemSize={3}
-          // usage={THREE.DynamicDrawUsage}
         />
       </bufferGeometry>
       <pointsMaterial
