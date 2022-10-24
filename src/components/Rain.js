a63ecfed8a47ce18998ca32f5f30dc114bdcb245
jmo5896 <justin.m.moore5896@gmail.com>
2022-10-24 11:04:08 -0500

    gets the rain drops to appear and hange static, now to get them to fall

--- src/components/Rain.js
+++ src/components/Rain.js
@@ -1,28 +1,34 @@
-import { Points } from "@react-three/drei";
 import React from "react";
 // rainCount start value 15000
 
 export default function Rain({ rainCount }) {
   //   console.log(rainCount);
-  const iterator = [...Array(rainCount).keys()];
-
+  const drops = new Float32Array(
+    [...Array(rainCount * 3).keys()].map((i) => {
+      if (i % 2 === 0) {
+        return Math.random() * 500 - 250;
+      }
+      return Math.random() * 400 - 200;
+    })
+  );
+  // console.log(drops);
   return (
-    <Points>
+    <points>
       <bufferGeometry>
         <bufferAttribute
           attach="attributes-position"
-          //   count={rainCount}
-          //   array={iterator.map(
-          //     (i) =>
-          //       new Float32Array([
-          //         Math.random() * 400 - 200,
-          //         Math.random() * 500 - 250,
-          //         Math.random() * 400 - 200,
-          //       ])
-          //   )}
+          count={drops.length / 3}
+          array={drops}
+          itemSize={3}
+          // usage={THREE.DynamicDrawUsage}
         />
       </bufferGeometry>
-      <pointsMaterial color={0xaaaaaa} size={0.1} transparent />
-    </Points>
+      <pointsMaterial
+        attach="material"
+        color={0xaaaaaa}
+        size={0.1}
+        transparent
+      />
+    </points>
   );
 }
