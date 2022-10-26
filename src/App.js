8e6fcfb87fb77b60005e38e9ae2db85681ce1a25
jmo5896 <justin.m.moore5896@gmail.com>
2022-10-26 12:11:09 -0500

    secret testing

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ import "./App.css";
 
 function App() {
   const [background, setBackground] = useState("storm");
-  console.log(process.env);
+  const keys = secrets || "dev mode";
+  console.log(keys);
   return (
     <div style={{ height: "100vh" }} className="App">
       <Toggle background={background} switchBackground={setBackground} />
